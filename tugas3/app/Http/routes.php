3dc7a1f0c92fc1e4156d6a73e1d83c3971c017f6
thaufiqumardi <thaufiqumardii@gmail.com>
2017-02-18 03:37:40 +0700

    editing commit

--- tugas3/app/Http/routes.php
+++ tugas3/app/Http/routes.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/','ViewController@index');
-//Route::get('/mahasiswa','Mahasiswa@index');
+Route::get('/mahasiswa','Mahasiswa@index');
 //Route::get('/mahasiswa','ViewController@mahasiswa');
