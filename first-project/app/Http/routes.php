50d885b4e13ac7a31bd1469db9f5d2a8a314bdc1
thaufiqumardi <thaufiqumardii@gmail.com>
2017-02-19 09:47:08 +0700

    Tugas 1 Membangun Project menggunakan Laravel

--- first-project/app/Http/routes.php
+++ first-project/app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/','ViewController@index');
-//Route::get('/mahasiswa','Mahasiswa@index');
-//Route::get('/mahasiswa','ViewController@mahasiswa');
+Route::get('/', function () {
+    return view('welcome');
+});
