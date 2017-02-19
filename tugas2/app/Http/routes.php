dbf1e7c64b0c1e7c900c007174c11f643a91c684
thaufiqumardi <thaufiqumardii@gmail.com>
2017-02-19 16:17:47 +0700

    Belajar Pakai controller (waktu kuliah di lab)

--- tugas2/app/Http/routes.php
+++ tugas2/app/Http/routes.php
@@ -13,6 +13,6 @@
 Route::get('/',function(){
 	return view('home');
 });
-// Route::get('/','ViewController@index');
+//Route::get('/','ViewController@index');
 //Route::get('/mahasiswa','Mahasiswa@index');
 //Route::get('/mahasiswa','ViewController@mahasiswa');
