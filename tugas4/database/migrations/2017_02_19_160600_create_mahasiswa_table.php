94bdaa9514b45f9019bafb2a5eda8d7a2d2c5f3c
thaufiqumardi <thaufiqumardii@gmail.com>
2017-02-19 23:34:03 +0700

    Migrate Database

--- tugas4/database/migrations/2017_02_19_160600_create_mahasiswa_table.php
+++ tugas4/database/migrations/2017_02_19_160600_create_mahasiswa_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMahasiswaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at');
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('nim');
+            $table->string('alamat');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::drop('mahasiswa');
     }
 }
